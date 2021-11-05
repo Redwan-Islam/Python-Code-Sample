0663e2757c61b8a2af04bc6c95e2cc8c43c5da92
Redwan-Islam <74811459+Redwan-Islam@users.noreply.github.com>
2021-11-05 19:40:55 +0600

    Add files via upload

---
+++ 03 - userInput.py
@@ -0,0 +1,8 @@
+name = input("What is your name? -->")
+age = input("What is your age? -->")
+place = input("Where do you live in? -->")
+
+
+print("your name is", name)
+print("You are", age , "years old")
+print("you live in", place)

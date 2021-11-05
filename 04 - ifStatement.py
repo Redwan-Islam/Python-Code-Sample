992e6cdf67ce8755f6b4b51e0f9494a205b84b74
Redwan-Islam <74811459+Redwan-Islam@users.noreply.github.com>
2021-11-05 19:46:21 +0600

    Add files via upload

---
+++ 04 - ifStatement.py
@@ -0,0 +1,6 @@
+fruit= ["apple","banana","orange","pineapple"]
+sampleInput = input("Enter a fruit name -->")
+if sampleInput in fruit:
+    print(sampleInput, "is in the fruit basket")
+else:
+    print("Not Found")
